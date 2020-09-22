0b9dc28db922227119705c27a53b8187c52cab1a
Kortney Stinson <kortneystinson@gmail.com>
2020-09-22 13:50:38 -0400

    Fix padding and margin on mobile 404 page for responsiveness

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,8 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div className="container-fluid">
-      <img src={require("../images/undraw_page_not_found_su7k.svg")} alt="404 Page Not Found"/>
+      <img src={require("../images/undraw_page_not_found_su7k.svg")} alt="404 Page Not Found" className="image-404"/>
+      <p>Sorry we couldn't find that page!</p>
     </div>
   </Layout>
 )
